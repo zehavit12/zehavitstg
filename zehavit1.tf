8dfe029d4571d1aa7b9db931d40d76bf1a3fc8bc
zehavit12 <95243588+zehavit12@users.noreply.github.com>
2022-02-10 14:46:30 +0200

    Update zehavit1.tf

--- zehavit1.tf
+++ zehavit1.tf
@@ -17,8 +17,7 @@ resource "aws_s3_bucket" "data" {
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
-    yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
-      
+    yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243
   })
 }
 
